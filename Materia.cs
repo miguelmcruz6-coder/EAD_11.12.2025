5ff838ad4234454b20fa41b8b54ed5cd414f2ced
Miguel Miyaki da Cruz - SENAI <miguel.m.cruz6@aluno.senai.br>
2025-12-11 10:41:51 -0300

    Update Materia.cs

--- Materia.cs
+++ Materia.cs
@@ -9,5 +9,20 @@ namespace EAD_11._12._2025
     {
         public string Nome { get; set; }
         public double Nota { get; set; }
+
+        public void MelhorNota(double maximo)
+        {
+            if (Nota == maximo)
+            {
+                Console.WriteLine($"Você teve a melhor nota em {Nome} com uma nota de {Nota}.");
+            }
+        }
+        public void MelhoresNotas(double maximo)
+        {
+            if (Nota == maximo)
+            {
+                Console.Write($"{Nome}, ");
+            }
+        }
     }
 }
