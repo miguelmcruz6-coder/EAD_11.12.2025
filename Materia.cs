5aea4e625d52d3b9b535851ca6bf70ff6557a7c8
Miguel Miyaki da Cruz - SENAI <miguel.m.cruz6@aluno.senai.br>
2025-12-11 10:21:19 -0300

    Add files via upload

---
+++ Materia.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EAD_11._12._2025
+{
+    public class Materia
+    {
+        public string Nome { get; set; }
+        public double Nota { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿
+using EAD_11._12._2025;
+
+List<Materia> materias = new List<Materia>();
+
+
+for (int i = 0; i < 5; i++)
+{
+    Materia materia = new Materia();
+    switch (i)
+    {
+        case 0:
+            materia.Nome = "Português";
+            Console.Write("\nDigite a nota em Português:\t");
+            materia.Nota = double.Parse(Console.ReadLine()!);
+            break;
+        case 1:
+            materia.Nome = "Matemática";
+            Console.Write("Digite a nota em Matemática:\t");
+            materia.Nota = double.Parse(Console.ReadLine()!);
+            break;
+        case 2:
+            materia.Nome = "Geografia";
+            Console.Write("Digite a nota em Geografia:\t");
+            materia.Nota = double.Parse(Console.ReadLine()!);
+            break;
+        case 3:
+            materia.Nome = "História";
+            Console.Write("Digite a nota em História:\t");
+            materia.Nota = double.Parse(Console.ReadLine()!);
+            break;
+        case 4:
+            materia.Nome = "Física";
+            Console.Write("Digite a nota em Física:\t");
+            materia.Nota = double.Parse(Console.ReadLine()!);
+            break;
+    }
+    materias.Add(materia);
+}
+
+double melhorNota = materias.Max(m => m.Nota);
+var materiasComMelhorNota = materias.Where(m => m.Nota == melhorNota).ToList();
+
+if (materiasComMelhorNota.Count == 1)
+{
+    Console.WriteLine($"\nA matéria com a melhor nota é {materiasComMelhorNota[0].Nome} com nota {materiasComMelhorNota[0].Nota}.\n");
+}
+else
+{
+    Console.WriteLine("\nHouve um empate entre as seguintes matérias com a melhor nota:\n");
+
+    Console.WriteLine("\nMatéria(s) com a melhor nota:\n");
+    foreach (var materia in materiasComMelhorNota)
+    {
+        Console.WriteLine($"{materia.Nome} - Nota: {materia.Nota}");
+    }
+    Console.WriteLine();
+}
